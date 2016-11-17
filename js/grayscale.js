4b0c5f73fc65cf09d221423b7fc5811ee6ad1109
Steve Venables <imsteve@hotmail.co.uk>
2016-11-17 23:20:09 +0000

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,8 +1,4 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
+
 
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
@@ -32,31 +28,23 @@ $('.navbar-collapse ul li a').click(function() {
     $(this).closest('.collapse').collapse('toggle');
 });
 
-// Google Maps Scripts
 var map = null;
-// When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
     map.setCenter(new google.maps.LatLng(50210069, -5.490848));
 });
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
         zoom: 15,
 
-        // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(50210069, -5.490848), // St Ives
 
-        // Disables the default Google Maps UI components
         disableDefaultUI: true,
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
             "elementType": "geometry",
@@ -167,14 +155,10 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
-    // Create the Google Map using out element and options defined above
     map = new google.maps.Map(mapElement, mapOptions);
 
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
     var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
     var beachMarker = new google.maps.Marker({
